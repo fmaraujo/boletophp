c609dc560e539c0ac3d6c536f4a15e1e14ac3dd0
Fernando Araujo <fernando@ipetecnologia.com.br>
2015-06-04 20:52:53 -0300

    ajustes gerais no layout do boleto do sicoob

--- include/funcoes_bancoob.php
+++ include/funcoes_bancoob.php
@@ -129,10 +129,10 @@ $altura = 50 ;
 
 
 //Guarda inicial
-?><img src=imagens/p.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
-src=imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
-src=imagens/p.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
-src=imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+?><img src=/img/boleto/p.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=/img/boleto/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=/img/boleto/p.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=/img/boleto/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
 <?php
 $texto = $valor ;
 if((strlen($texto) % 2) <> 0){
@@ -151,7 +151,7 @@ while (strlen($texto) > 0) {
       $f1 = $largo ;
     }
 ?>
-    src=imagens/p.png width=<?php echo $f1?> height=<?php echo $altura?> border=0><img 
+    src=/img/boleto/p.png width=<?php echo $f1?> height=<?php echo $altura?> border=0><img 
 <?php
     if (substr($f,$i,1) == "0") {
       $f2 = $fino ;
@@ -159,16 +159,16 @@ while (strlen($texto) > 0) {
       $f2 = $largo ;
     }
 ?>
-    src=imagens/b.png width=<?php echo $f2?> height=<?php echo $altura?> border=0><img 
+    src=/img/boleto/b.png width=<?php echo $f2?> height=<?php echo $altura?> border=0><img 
 <?php
   }
 }
 
 // Draw guarda final
 ?>
-src=imagens/p.png width=<?php echo $largo?> height=<?php echo $altura?> border=0><img 
-src=imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
-src=imagens/p.png width=<?php echo 1?> height=<?php echo $altura?> border=0> 
+src=/img/boleto/p.png width=<?php echo $largo?> height=<?php echo $altura?> border=0><img 
+src=/img/boleto/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=/img/boleto/p.png width=<?php echo 1?> height=<?php echo $altura?> border=0> 
   <?php
 } //Fim da função
 
